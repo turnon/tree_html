c62dbec2ebb28554f429d5c633ba5d77c5c8d0cb
ken <block24block@gmail.com>
2017-11-22 23:56:28 +0800

    read main css/js once

--- lib/tree_html.rb
+++ lib/tree_html.rb
@@ -6,6 +6,8 @@ module TreeHtml
   NO_CHECKBOX = "<label class='placeholder'></label>".freeze
   BLANK = ''.freeze
   NO_CUSTOM_JS = [].freeze
+  Css = File.read File.expand_path('../tree_html/tree_html.css', __FILE__)
+  Js = File.read File.expand_path('../tree_html/tree_html.js', __FILE__)
 
   def tree_html
     "<ul class='tree-html'>#{li_for_tree_html}</ul>"
@@ -15,19 +17,16 @@ module TreeHtml
     "<!DOCTYPE HTML><html>"\
       "<head>"\
         "<meta charset='utf-8'/>"\
-        "<style>#{main_css + css_for_tree_html}</style>"\
+        "<style>#{Css + css_for_tree_html}</style>"\
         "#{custom_js head_js_for_tree_html}"\
       "</head>"\
       "<body>"\
         "#{tree_html}"\
-        "<script>#{main_js}</script>"\
+        "<script>#{Js}</script>"\
         "#{custom_js body_js_for_tree_html}"\
       "</body>"\
     "</html>"
   end
-
-  Css = File.expand_path('../tree_html/tree_html.css', __FILE__)
-  Js = File.expand_path('../tree_html/tree_html.js', __FILE__)
 
   protected
 
@@ -52,14 +51,6 @@ module TreeHtml
   end
 
   private
-
-  def main_css
-    File.read(Css)
-  end
-
-  def main_js
-    File.read(Js)
-  end
 
   def sub_ul
     if children_for_tree_html.empty?
